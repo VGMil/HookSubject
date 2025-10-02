76ba5eba3d9b28a174d1f97c07a16fd07180395b
VGMil <vgmiltonisaac@gmail.com>
2025-10-01 22:45:08 -0500

    💄ui(index): modificar el linear gradient - se modifica el linear gradient para que se vea mejor entre blancos y transparentes

--- app/index.tsx
+++ app/index.tsx
@@ -9,11 +9,11 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function Index() {
   const image = { 'uri': require('../assets/bg/background.png') };
   return (
-    <ImageBackground source={image.uri} resizeMode="cover" className="w-full h-full ">
+    <ImageBackground source={image.uri} resizeMode="cover" className="w-full h-full">
       <LinearGradient
-        colors={['transparent', 'rgba(255,255,255,0.9)']}
-        locations={[0, 0.6]}
-        className="w-full h-full items-end justify-end"
+        colors={['rgba(255,255,255,1)','transparent','transparent', 'rgba(255,255,255,1)']}
+        locations={[0.01, 0.15,0.5, 0.7]}
+        className="flex-1 items-end justify-end"
       >
         <SafeAreaView className="items-start justify-start flex-col w-full h-2/5 px-8 gap-4" >
           <CustomText size='large' bold className='h-20'>Regresaste!</CustomText> 
@@ -25,7 +25,7 @@ export default function Index() {
             <CustomButton onPress={() => {}} variant='primary' >Registrarse</CustomButton>
           </View>
         </SafeAreaView>
-      </LinearGradient>
+        </LinearGradient>
     </ImageBackground>
   );
 }
