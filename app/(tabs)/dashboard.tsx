19b90db4b19645a734ba4a2c90cdb382dbde35fa
vgmil <vgmiltonisaac@gmail.com>
2025-10-05 21:35:45 -0500

    ✨feat(dashboard): Actualización del Dashboard
    - Se ha introducido un Card para mostrar información de ejemplo acerca de las finanzas
    - Se mantiene con variables aquellos datos a enlazar.

--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -2,11 +2,15 @@ import React, { useCallback } from 'react';
 import { Dimensions, View } from 'react-native';
 
 import BackGround from '@/components/ui/BackGround';
+import CustomCard from '@/components/ui/CustomCard';
+import { CustomText } from '@/components/ui/CustomText';
+import Feather from '@expo/vector-icons/Feather';
 import { Easing, withRepeat, withSequence, withTiming } from 'react-native-reanimated';
 const image = require('@/assets/bg/login.png')
 const HEIGHT = Dimensions.get('window').height;
 const WIDTH = Dimensions.get('window').width;
-
+const fecha = "Viernes, 3 de Octubre"
+const money = 1024.34
 const Dashboard = () => {
 
 const backGroundAnimation = useCallback(() => {
@@ -27,14 +31,24 @@ const backGroundAnimation = useCallback(() => {
     );
 }, []);
   return (
-    <View>
+    <View className='flex-1 items-center justify-start gap-4'>
       <BackGround
         image={image}
         height={HEIGHT}
         width={WIDTH}
-        opacity={0.5}
+        opacity={0.1}
         animation={backGroundAnimation}>
       </BackGround>
+      <CustomCard className='gap-2'>
+        <View className='flex-row items-center justify-between'>
+          <CustomText size='medium' color='black'>{fecha}</CustomText>
+          <Feather name="lock" size={16} color="gray" />
+        </View>
+        <CustomText size='large' color='black' bold className='mb-4'>Mi economia</CustomText>
+        <CustomText size="medium">Saldo Actual:</CustomText>
+        <CustomText size='large' color='black' bold>{`$${money}`}</CustomText>
+        <CustomText size='small' color='gray'>{`Presupuesto Mensual: $${money}`}</CustomText>
+      </CustomCard>
     </View>
   )
 }
